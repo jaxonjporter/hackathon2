c3c813aba94819404ae6fd74377eb84d33fee645
bweegee <bw33g33@gmail.com>
2019-04-06 14:17:40 -0600

    axios call fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
 
   namespace :api do
-  resources :videos do
-    resources :comments
-  end
+    resources :videos do
+      resources :comments
+    end
   end
 end
