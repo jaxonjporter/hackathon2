637435965faa9489e81e8c8907d72301d6388d23
Jaxon Porter <jaxonutah@gmail.com>
2019-04-06 11:09:55 -0600

    adding seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
 5.times do
   user = User.create(
-    name: Faker::Name.last_name,
+    name: Faker::Name.name,
+    nickname: Faker::Name.middle_name,
+    email: Faker::Internet.email,
+    password: 'password',
+  )
+  2.times do
+    v = Video.create(
+      title: Faker::Music::RockBand.name,
+      video_url: 'https://youtu.be/dQw4w9WgXcQ',
+      user_id: user.id
+    )
+    3.times do
+      c = Comment.create(
+        title: Faker::Movies::Hobbit.location,
+        body: Faker::TvShows::MichaelScott.quote,
+        user_id: user.id,
+        video_id: v.id
+      )
+    end
+  end
+end
+
+1.times do
+  user = User.create(
+    name: Faker::Name.name,
+    nickname: Faker::Name.middle_name,
     email: 'test@test.com',
     password: 'password',
-    admin: true
   )
+  2.times do
+    v = Video.create(
+      title: Faker::Music::RockBand.name,
+      video_url: 'https://youtu.be/dQw4w9WgXcQ',
+      user_id: user.id
+    )
+    3.times do
+      c = Comment.create(
+        title: Faker::Movies::Hobbit.location,
+        body: Faker::TvShows::MichaelScott.quote,
+        user_id: user.id,
+        video_id: v.id
+      )
+    end
+  end
+end
+
+puts 'seeds planted'
