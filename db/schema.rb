b9398b0be3411afb658e92f6aa98df4281ae5f6d
Nathan Swift <nswiftnswift@gmail.com>
2019-04-06 14:58:50 -0600

    init playlist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_06_161656) do
+ActiveRecord::Schema.define(version: 2019_04_06_205710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2019_04_06_161656) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_comments_on_user_id"
     t.index ["video_id"], name: "index_comments_on_video_id"
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -71,5 +79,6 @@ ActiveRecord::Schema.define(version: 2019_04_06_161656) do
 
   add_foreign_key "comments", "users"
   add_foreign_key "comments", "videos"
+  add_foreign_key "playlists", "users"
   add_foreign_key "videos", "users"
 end
