fe5f1adbefdd78b59ba772a59120fd96af0a786a
Nathan Swift <nswiftnswift@gmail.com>
2019-04-06 16:11:15 -0600

    init axios

--- client/src/components/PlaylistShow.js
+++ client/src/components/PlaylistShow.js
@@ -2,21 +2,27 @@ import React from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { Card, Image } from 'semantic-ui-react'
+import VideoShow from './VideoShow'
 
 class PlaylistShow extends React.Component {
   state = { videos: [] }
 
   componentDidMount() {
-    axios.get("/api/videos")
+    debugger
+    const { match: { params: { id, video_id } } } = this.props
+    axios.get(`/api/videos/${video_id}/comments`)
+      .then( res => this.setState({ comments: res.data, }))
+    axios.get("/api/videos/")
       .then( res => this.setState({ videos: res.data, }))
-}
+  }
+
 
 renderVideos = () => {
   const { videos } = this.state
 
   return(
   videos.map( video => (
-        <Link to='/VideoShow'>
+        <Link to={VideoShow}>
           <Card style={{ width: '150px', height: '100px'}}>
             <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
             <Card.Content>
