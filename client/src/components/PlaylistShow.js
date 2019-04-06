bd7c453b60469713a5590df2795164acb2eea433
Nathan Swift <nswiftnswift@gmail.com>
2019-04-06 16:54:06 -0600

    inc counter

--- client/src/components/PlaylistShow.js
+++ client/src/components/PlaylistShow.js
@@ -1,44 +1,44 @@
 import React from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-import { Card, Image } from 'semantic-ui-react'
-import VideoShow from './VideoShow'
+import { Container, Card, Image } from 'semantic-ui-react';
 
-class PlaylistShow extends React.Component {
-  state = { videos: [] }
-
-  componentDidMount() {
-    debugger
-    const { match: { params: { id, video_id } } } = this.props
-    axios.get(`/api/videos/${video_id}/comments`)
-      .then( res => this.setState({ comments: res.data, }))
-    axios.get("/api/videos/")
-      .then( res => this.setState({ videos: res.data, }))
+class PlaylistShow extends React.Component{
+  state = {
+    videos: [],
   }
 
-
-renderVideos = () => {
-  const { videos } = this.state
-
-  return(
-  videos.map( video => (
-        <Link to={VideoShow}>
-          <Card style={{ width: '150px', height: '100px'}}>
-            <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
-            <Card.Content>
-            <Card.Header>{video.title}</Card.Header>
-            </Card.Content>
-          </Card>
-        </Link>
-  )
-  ))
+componentDidMount() {
+    axios.get("/api/videos")
+      .then( res => this.setState({ videos: res.data, }))
 }
 
-  render () {
+  renderVideos = () => {
+    const { videos } = this.state
+
     return(
-      <div> { this.renderVideos() }</div>
+    videos.map( video => (
+          <Link to={`/videos/${video.id}/VideoShow`} key={video.id}>
+            <Card style={{ width: '150px', height: '100px'}}>
+              <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' />
+              <Card.Content>
+              <Card.Header>{video.title}</Card.Header>
+              </Card.Content>
+            </Card>
+          </Link>
+    )
+    ))
+  }
+
+  render () {
+    return (
+      <Container>
+        <Card.Group itemsPerRow={4}>
+          {this.renderVideos()}
+        </Card.Group>
+      </Container>
     )
   }
 }
 
-export default PlaylistShow
+export default PlaylistShow;
